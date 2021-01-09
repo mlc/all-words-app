3cc02871e1ec0e9e4e041c88a6ccc237ef9d286c
mike castleman <m@mlcastle.net>
2021-01-09 18:39:40 -0500

    upload

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -ex
+
+rm -r dist
+yarn build
+aws s3 cp dist/*.css s3://words.oulipo.link/ --content-type 'text/css' --cache-control 'public,max-age=31556952,immutable'
+aws s3 cp dist/*.js s3://words.oulipo.link/ --content-type 'application/javascript' --cache-control 'public,max-age=31556952,immutable'
+aws s3 cp dist/*.map s3://words.oulipo.link/ --content-type 'application/json' --cache-control 'public,max-age=31556952,immutable'
+aws s3 cp dist/index.html s3://words.oulipo.link/index.html --cache-control 'public,max-age=3600' --content-language 'en-US' --content-type 'text/html;charset=utf-8'
+aws cloudfront create-invalidation --distribution-id E3NY7VRACZU7ZT --paths '/' '/index.html'
+
